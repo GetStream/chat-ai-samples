842a1b064a991774b6c138e957c5225f456e920b
skydoves <skydoves@naver.com>
2024-12-05 13:39:44 +0900

    Add Android Compose AI sample project

---
+++ android/settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+  repositories {
+    google {
+      content {
+        includeGroupByRegex("com\\.android.*")
+        includeGroupByRegex("com\\.google.*")
+        includeGroupByRegex("androidx.*")
+      }
+    }
+    mavenCentral()
+    gradlePluginPortal()
+    maven(url = "https://jitpack.io")
+    maven(url="https://oss.sonatype.org/content/repositories/snapshots/")
+  }
+}
+dependencyResolutionManagement {
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+  repositories {
+    google()
+    mavenCentral()
+    maven(url = "https://jitpack.io")
+    maven(url="https://oss.sonatype.org/content/repositories/snapshots/")
+  }
+}
+
+rootProject.name = "ai-assistant-android"
+include(":app")

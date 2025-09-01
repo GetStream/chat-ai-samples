b0093dbfe6709f41cd47684f044457a9b5004ecd
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-09-01 21:01:36 +0200

    chore: update react native sample

--- react-native/contexts/AppContext.tsx
+++ react-native/contexts/AppContext.tsx
@@ -2,21 +2,21 @@ import React, { ReactNode, useState } from 'react';
 import type { Channel } from 'stream-chat';
 
 export type AppContextValue = {
-  channel: Channel | undefined;
-  setChannel: (channel: Channel) => void;
+    channel: Channel | undefined;
+    setChannel: (channel: Channel) => void;
 };
 
 export const AppContext = React.createContext<AppContextValue>({
-  setChannel: () => {},
-  channel: undefined,
+    setChannel: () => {},
+    channel: undefined,
 });
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
-  const [channel, setChannel] = useState<Channel>();
+    const [channel, setChannel] = useState<Channel>();
 
-  const contextValue = { channel, setChannel };
+    const contextValue = { channel, setChannel };
 
-  return <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>;
+    return <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => React.useContext(AppContext);

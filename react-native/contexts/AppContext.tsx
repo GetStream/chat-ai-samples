d2958d6d707f36e25c97ac82645975d6ee5629d1
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-12-04 15:18:21 +0100

    chore: cleanup of inconsistent code

--- react-native/contexts/AppContext.tsx
+++ react-native/contexts/AppContext.tsx
@@ -13,32 +13,32 @@ export const AppContext = React.createContext<AppContextValue>({
 });
 
 const ALPHABET =
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-';
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-';
 
-export function nanoid(size: number = 21): string {
+export const nanoid = (size: number = 21): string => {
   let id = '';
   for (let i = 0; i < size; i++) {
     const r = Math.floor(Math.random() * ALPHABET.length);
     id += ALPHABET[r];
   }
   return id;
-}
+};
 
 export const createChannel = (client: StreamChat) =>
-  client.channel('messaging', nanoid(), {
-    members: [chatUserId],
-  });
+    client.channel('messaging', nanoid(), {
+      members: [chatUserId],
+    });
 
 export const AppProvider = ({
-  client,
-  children,
-}: PropsWithChildren<{ client: StreamChat }>) => {
+                              client,
+                              children,
+                            }: PropsWithChildren<{ client: StreamChat }>) => {
   const [channel, setChannel] = useState<Channel>(() => createChannel(client));
 
   const contextValue = useMemo(() => ({ channel, setChannel }), [channel]);
 
   return (
-    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+      <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
   );
 };
 

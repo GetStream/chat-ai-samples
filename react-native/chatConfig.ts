c3da331fbeb04a305d793601b62e484c84c9d14f
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-12-04 17:54:26 +0100

    chore: change demo user for RN sample app

--- react-native/chatConfig.ts
+++ react-native/chatConfig.ts
@@ -1,4 +1,4 @@
 export const chatApiKey = 'zcgvnykxsfm8';
-export const chatUserId = 'rn-ai-test-user';
-export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm4tYWktdGVzdC11c2VyIn0.5pc_6W73-BxPxDbnnK3fx-F8WUb6lXxBOiq7IiIbaS4';
+export const chatUserId = 'rn-chatgpt-demo-test-user';
+export const chatUserToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoicm4tY2hhdGdwdC1kZW1vLXRlc3QtdXNlciJ9.cE-FOtn8jz7k4ddRJ1kCA7g456tTEwxXEn4adka1p2s';
 export const chatUserName = 'Bob';

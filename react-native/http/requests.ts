5370ff8648810495bb5c22f9b6f2ff3932353068
Ivan Sekovanikj <ivan.sekovanikj@getstream.io>
2025-12-04 09:41:04 +0100

    chore(react-native): use deployed version of the backend

--- react-native/http/requests.ts
+++ react-native/http/requests.ts
@@ -1,8 +1,15 @@
 import { post } from './api.ts';
 
 export const startAI = async (channelId: string) =>
-  post('http://192.168.1.80:3000/start-ai-agent', { channel_id: channelId });
+    post('https://ai-sdk-server-0f347d455e2e.herokuapp.com/start-ai-agent', { channel_id: channelId });
 export const stopAI = async (channelId: string) =>
-  post('http://192.168.1.80:3000/stop-ai-agent', { channel_id: channelId });
+    post('https://ai-sdk-server-0f347d455e2e.herokuapp.com/stop-ai-agent', { channel_id: channelId });
 export const summarize = async (text: string) =>
-  post('http://192.168.1.80:3000/summarize', { text });
+    post('https://ai-sdk-server-0f347d455e2e.herokuapp.com/summarize', { text });
+
+// export const startAI = async (channelId: string) =>
+//     post('http://localhost:3000/start-ai-agent', { channel_id: channelId });
+// export const stopAI = async (channelId: string) =>
+//     post('http://localhost:3000/stop-ai-agent', { channel_id: channelId });
+// export const summarize = async (text: string) =>
+//     post('http://localhost:3000/summarize', { text });
